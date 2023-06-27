ef41c8d2b72d3cbf563ef4c914a57001b0907afb
nucikk <kirianuci@gmail.com>
2023-06-27 15:26:29 +0400

    add game name change functionality

--- subscribe/src/HomeSub.jsx
+++ subscribe/src/HomeSub.jsx
@@ -1,11 +1,24 @@
+import { useState } from "react";
 
 const HomeSub = () => {
+  const [gameName, setGameName] = useState("");
+
+//თამაშის სახელის განახლების ფუნქცია
+  function handleChangeText(e) {
+    e.preventDefault();
+    setGameName("Let's start the game:");
+  }
   return (
     <>
-        <h1>Game Development Subscribe</h1>
-       <div className="subscribe_card"></div>
+      <h1 className="subscribe_title">Game Development Subscribe</h1>
+      <form action="" onSubmit={handleChangeText} className="subscribe_form">
+        <div>
+          <h3 className="subscribe_about">how to {gameName} </h3>
+          <button>more</button>
+        </div>
+      </form>
     </>
-  )
-}
+  );
+};
 
-export default HomeSub
+export default HomeSub;
