90e9f5d3a6d6582f3f919fd6a28c3b69066d022f
nucikk <kirianuci@gmail.com>
2023-06-27 14:42:07 +0400

    add init subscribe app

---
+++ subscribe/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import HomeSub from './HomeSub'
+
+function App() {
+ 
+
+  return (
+    <>
+      <HomeSub />
+    </>
+  )
+}
+
+export default App
---
+++ subscribe/src/HomeSub.jsx
@@ -0,0 +1,11 @@
+
+const HomeSub = () => {
+  return (
+    <>
+        <h1>Game Development Subscribe</h1>
+       <div className="subscribe_card"></div>
+    </>
+  )
+}
+
+export default HomeSub
